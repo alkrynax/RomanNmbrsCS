ca5b9f755504ad047f8835fd90cc85ce08786bd5
aymericbarbot <144786388+aymericbarbot@users.noreply.github.com>
2023-10-25 11:07:55 +0200

    Delete NombresRomains directory

--- NombresRomains/ConvertisseurNombresRomains.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace NombresRomains;
-
-public static class ConvertisseurNombresRomains
-{
-    public static string Convertir(uint chiffre)
-    {
-        return chiffre == 1 ? "I" : "II";
-    }
-}
